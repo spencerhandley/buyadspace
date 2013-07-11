d522a37e5bce65bab6d5c0ef551bccf0efd8e775
spencer handley <spencerhandley@gmail.com>
2013-07-11 10:59:02 -0700

    Add repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buyadspace::Application.config.secret_token = '1d7048e8868581de227aa2dde310e3da985b719ad03fa0243545d7322fa910b7f5f278f8dd0cd7cbc3a6f213355577b8e308fb2ac3bf6cc886cee8691b059685'
